f0b7cbe760da1e969e7132011427c24d70d49dd9
hernandez87v <hernandez87v@hotmail.com>
2020-05-05 13:22:26 -0700

    Completed Model spec: User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-    has_secure_password
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, confirmation: true, length: { minimum: 4 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.where("LOWER(email) = ?", email.downcase.strip)[0].try(:authenticate, password)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+  
+  subject {
+    described_class.new(email: "TEST@TEST.com", first_name: "JOE", last_name: "SHMO", password: "12345", password_confirmation: "12345")
+  }
+  
   describe 'Validations' do
-    # validation examples here
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid when password confirmation does not match' do
+      subject.password_confirmation = "1123513"
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid when password not given in both fields' do
+      subject.password_confirmation = nil
+      expect(subject).to be_invalid
+    end
+
+    it "is invalid when email already exists in the database (not case sensitive)" do
+      subject.save!
+      @user = User.new(email: "test@test.COM", first_name: "JOE", last_name: "SHMO", password: "12345", password_confirmation: "1234")
+      expect(@user).to be_invalid
+    end
+
+    it "is invalid when email not given" do
+      subject.email = nil
+      expect(subject).to be_invalid
+    end
+
+    it "is invalid when first_name not given" do
+      subject.first_name = nil
+      expect(subject).to be_invalid
+    end
+
+    it "is invalid when last_name not given" do
+      subject.last_name = nil
+      expect(subject).to be_invalid
+    end
+    
+    it "is invalid when password length is < 4" do
+      subject.password = "1"
+      subject.password_confirmation = "1"
+      expect(subject).to be_invalid
+    end
   end
 
-  describe '.authenticate_with_credentials' do
-    # examples for this class method here
-  end
 
+  describe ".authenticate_with_credentials" do
+    before(:each) do 
+      subject.save!
+    end
+
+    it "should be valid if credentials match" do
+      user = User.authenticate_with_credentials(subject.email, subject.password)
+      expect(user).to be_instance_of(User)
+      expect(user.id).to eq(subject.id)
+    end
+
+    it "should be invalid if password doesn't match" do
+      user = User.authenticate_with_credentials(subject.email, "8957452")
+      expect(user).not_to be_instance_of(User)
+    end
+
+    it "should be invalid if email doesn't match" do
+      user = User.authenticate_with_credentials("hello@yahoo.com", subject.password)
+       expect(user).not_to be_instance_of(User)
+    end
+
+    it "should be valid if email has trailing spaces" do
+      user = User.authenticate_with_credentials("  TEST@TEST.com  ", subject.password)
+      expect(user).to be_instance_of(User)
+      expect(user.id).to eq(subject.id)
+    end
+
+    it "should be valid if email cases do not match exactly" do
+      user = User.authenticate_with_credentials("test@test.com", subject.password)
+      expect(user).to be_instance_of(User)
+      expect(user.id).to eq(subject.id)
+    end
+  end
 end
