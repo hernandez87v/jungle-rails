16fea238b23bca3b612bbc188e27a3d312cc5301
hernandez87v <hernandez87v@hotmail.com>
2020-05-05 15:23:21 -0700

    Feature Spec: Home Page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,7 +11,6 @@ require 'rspec/rails'
 require "capybara/rails"
 require "capybara/rspec"
 require "capybara/poltergeist" # Add this line to require poltergeist
-
 # Specs flagged with `js: true` will use Capybara's JS driver. Set
 # that JS driver to :poltergeist
 Capybara.javascript_driver = :poltergeist
